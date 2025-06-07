abd5844876409aa504543c86d7a8bcaa3590f543
kavyaks99 <kskavya3661@gmail.com>
2025-06-07 19:47:29 +0530

    Add .gitignore file

--- node_modules/livekit-client/dist/ts4.2/src/connectionHelper/checks/publishVideo.d.ts
+++
@@ -1,6 +0,0 @@
-import { Checker } from './Checker';
-export declare class PublishVideoCheck extends Checker {
-    get description(): string;
-    perform(): Promise<void>;
-}
-//# sourceMappingURL=publishVideo.d.ts.map
